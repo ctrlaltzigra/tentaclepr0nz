88dbca4652e6bea6c49517985c45777c6e8e1c43
Zigra V <zigra@protonmail.com>
2021-08-07 17:07:52 -0400

    seashellz.py
    Here's my first python script, lol.
    Was supposed to be Shabbat but I wanted to code v badly

---
+++ seashellz.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print('Here is my first Python Script.')
+howManySeashellsEqualADollar = 10
+howManySeashellsIHave = 10000
+usd = howManySeashellsEqualADollar * howManySeashellsIHave
+print('Here is how much my seashell collection is worth:')
+print(usd)
